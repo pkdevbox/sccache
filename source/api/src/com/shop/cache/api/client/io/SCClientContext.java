0abc5183e64437e4375e9445c55d9502130e75e8
Randgalt <Randgalt@2f957966-b9ea-11dd-893d-d98698f66bbf>
2008-12-16 22:12:30 +0000

    initial

---
+++ source/api/src/com/shop/cache/api/client/io/SCClientContext.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2008 SHOP.COM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.shop.cache.api.client.io;
+
+import com.shop.cache.api.common.SCNotifications;
+import java.net.InetSocketAddress;
+
+/**
+ * Builder interface for containing client data. NOTE: it is not necessary to set every
+ * value. Implementations should provide reasonable defaults (except for the client address)
+ *
+ * @author Jordan Zimmerman
+ */
+public interface SCClientContext
+{
+	/**
+	 * Set the address of the client
+	 *
+	 * @param address address (host and port)
+	 * @return this
+	 */
+	public SCClientContext			address(InetSocketAddress address);
+
+	/**
+	 * Change the value used to maintain heartbeats between client and server. NOTE: this must
+	 * be coordinated with the server context
+	 *
+	 * @param s new hearbeat
+	 * @return this
+	 */
+	public SCClientContext 			heartbeat(String s);
+
+	/**
+	 * Set a notification object
+	 *
+	 * @param n new notification object
+	 * @return tbis
+	 */
+	public SCClientContext 			notificationHandler(SCNotifications n);
+
+	/**
+	 * Returns the currently set address
+	 *
+	 * @return address
+	 */
+	public InetSocketAddress		getAddress();
+
+	/**
+	 * Returns the currently set heartbeat
+	 *
+	 * @return heartbeat
+	 */
+	public String					getHeartbeat();
+
+	/**
+	 * Returns the current notification handler
+	 *
+	 * @return handler or null
+	 */
+	public SCNotifications 			getNotificationHandler();
+}
---
+++ source/api/src/com/shop/cache/api/client/main/DefaultMemoryCache.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2008 SHOP.COM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.shop.cache.api.client.main;
+
+import com.shop.cache.api.client.io.SCManager;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * The default memory cache implementation. This implementation
+ * is based on SoftReferences. It does not limit the number of
+ * objects cached in memory. Instead it relies on the JVM to handle
+ * the SoftReferences.
+ *
+ * @author Jordan Zimmerman
+ */
+class DefaultMemoryCache implements SCMemoryCache
+{
+	/**
+	 * Link back to the manager being used
+	 *
+	 * @param manager the manager
+	 */
+	DefaultMemoryCache(SCManager manager)
+	{
+		fManager = manager;
+		fMemoryCache = new ConcurrentHashMap<String, SoftReference<SCDataBlock>>();
+		fQueue = new ReferenceQueue<SCDataBlock>();
+
+		Thread		queueThread = new Thread
+		(
+			new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					referencePurgeLoop();
+				}
+			},
+			"DefaultMemoryCache reference thread"
+		);
+		queueThread.setDaemon(true);
+		queueThread.start();
+
+		Thread		purgeThread = new Thread
+		(
+			new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					stalePurgeLoop();
+				}
+			},
+			"DefaultMemoryCache purge thread"
+		);
+		purgeThread.setDaemon(true);
+		purgeThread.start();
+	}
+
+	@Override
+	public void put(SCDataBlock block)
+	{
+		fMemoryCache.put(block.getKey(), new SoftReference<SCDataBlock>(block, fQueue));
+	}
+
+	@Override
+	public SCDataBlock get(String key)
+	{
+		SoftReference<SCDataBlock> 		memoryBlockRef = fMemoryCache.get(key);
+		return (memoryBlockRef != null) ? memoryBlockRef.get() : null;
+	}
+
+	/**
+	 * DefaultMemoryCache uses a background thread that periodically checks the TTL of
+	 * objects in memory against the main cache. Stale objects are removed from memory
+	 */
+	private void	stalePurgeLoop()
+	{
+		for(;;)
+		{
+			try
+			{
+				Thread.sleep(STALE_PURGE_SLEEP_TICKS);
+			}
+			catch ( InterruptedException e )
+			{
+				Thread.currentThread().interrupt();
+				break;
+			}
+
+			for ( String key : fMemoryCache.keySet() )
+			{
+				SoftReference<SCDataBlock> 	memoryBlockRef = fMemoryCache.get(key);
+				if ( memoryBlockRef != null )
+				{
+					SCDataBlock		block = memoryBlockRef.get();
+					if ( block != null )
+					{
+						try
+						{
+							long 			ttl = fManager.getTTL(key);
+							if ( (ttl == 0) || (ttl > block.getTTL()) )	// TTL has changed - purge from memory so that the correct copy is retrieved
+							{
+								fMemoryCache.remove(key);
+							}
+						}
+						catch ( Exception e )
+						{
+							if ( fManager.getNotificationHandler() != null )
+							{
+								fManager.getNotificationHandler().notifyException("DefaultMemoryCache.stalePurgeLoop()", e);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private void referencePurgeLoop()
+	{
+		try
+		{
+			for(;;)
+			{
+				Reference<? extends SCDataBlock>	ref = fQueue.remove();
+				SCDataBlock 						block = ref.get();
+				if ( block != null )
+				{
+					fMemoryCache.remove(block.getKey());
+				}
+			}
+		}
+		catch ( InterruptedException e )
+		{
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private static final int			STALE_PURGE_SLEEP_TICKS = 60 * 1000;	// 1 minute
+
+	private final ConcurrentHashMap<String, SoftReference<SCDataBlock>>	fMemoryCache;
+	private final ReferenceQueue<SCDataBlock>							fQueue;
+	private final SCManager 											fManager;
+}
