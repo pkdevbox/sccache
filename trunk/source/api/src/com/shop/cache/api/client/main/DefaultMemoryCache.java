3e0107d534cfc22e13982221899d36e63777134b
Randgalt <Randgalt@2f957966-b9ea-11dd-893d-d98698f66bbf>
2009-01-31 01:12:52 +0000

    I found out the ConcurrentHashMap.keySet() can lock references to the values in the map. This creates a potential for OOMs in DefaultMemoryCache. I've worked around it by using a separate Set to hold the keys.

--- trunk/source/api/src/com/shop/cache/api/client/main/DefaultMemoryCache.java
+++ trunk/source/api/src/com/shop/cache/api/client/main/DefaultMemoryCache.java
@@ -19,6 +19,8 @@ import com.shop.cache.api.client.io.SCManager;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
+import java.util.Collections;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -40,6 +42,7 @@ class DefaultMemoryCache implements SCMemoryCache
 	{
 		fManager = manager;
 		fMemoryCache = new ConcurrentHashMap<String, SoftReference<SCDataBlock>>();
+		fMemoryCacheKeys = Collections.newSetFromMap(new ConcurrentHashMap());
 		fQueue = new ReferenceQueue<SCDataBlock>();
 
 		Thread		queueThread = new Thread
@@ -77,6 +80,7 @@ class DefaultMemoryCache implements SCMemoryCache
 	public void put(SCDataBlock block)
 	{
 		fMemoryCache.put(block.getKey(), new SoftReference<SCDataBlock>(block, fQueue));
+		fMemoryCacheKeys.add(block.getKey());
 	}
 
 	@Override
@@ -104,7 +108,9 @@ class DefaultMemoryCache implements SCMemoryCache
 				break;
 			}
 
-			for ( String key : fMemoryCache.keySet() )
+			// calling ConcurrentHashMap.keySet() ends up locking references to much of the cache
+			// which can cause an OutOfMemoryException. So, use a separate/parallel set for the keys
+			for ( String key : fMemoryCacheKeys )
 			{
 				SoftReference<SCDataBlock> 	memoryBlockRef = fMemoryCache.get(key);
 				if ( memoryBlockRef != null )
@@ -118,6 +124,7 @@ class DefaultMemoryCache implements SCMemoryCache
 							if ( (ttl == 0) || (ttl > block.getTTL()) )	// TTL has changed - purge from memory so that the correct copy is retrieved
 							{
 								fMemoryCache.remove(key);
+								fMemoryCacheKeys.remove(key);
 							}
 						}
 						catch ( Exception e )
@@ -144,6 +151,7 @@ class DefaultMemoryCache implements SCMemoryCache
 				if ( block != null )
 				{
 					fMemoryCache.remove(block.getKey());
+					fMemoryCacheKeys.remove(block.getKey());
 				}
 			}
 		}
@@ -156,6 +164,7 @@ class DefaultMemoryCache implements SCMemoryCache
 	private static final int			STALE_PURGE_SLEEP_TICKS = 60 * 1000;	// 1 minute
 
 	private final ConcurrentHashMap<String, SoftReference<SCDataBlock>>	fMemoryCache;
+	private final Set<String> 											fMemoryCacheKeys;
 	private final ReferenceQueue<SCDataBlock>							fQueue;
 	private final SCManager 											fManager;
 }
